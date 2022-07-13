79ff2ea0e1a1f9ef34a63bc8cb414c257af2eee4
arpit016 <agarwalarpit22@gmail.com>
2022-07-13 16:14:53 +0530

    Added tiptap

---
+++ src/Tiptap.jsx
@@ -0,0 +1,306 @@
+import React, { useCallback } from "react";
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import Placeholder from '@tiptap/extension-placeholder';
+import Underline from "@tiptap/extension-underline";
+import Link from '@tiptap/extension-link';
+import Superscript from "@tiptap/extension-superscript";
+import Subscript from "@tiptap/extension-subscript";
+import TextStyle from '@tiptap/extension-text-style';
+import { Color } from '@tiptap/extension-color';
+import Highlight from '@tiptap/extension-highlight';
+import TextAlign from '@tiptap/extension-text-align'
+import {
+  FaBold,
+  FaHeading,
+  FaItalic,
+  FaListOl,
+  FaListUl,
+  FaQuoteLeft,
+  FaRedo,
+  FaStrikethrough,
+  FaUnderline,
+  FaUndo,
+  FaCode,
+  FaSubscript,
+  FaSuperscript
+} from "react-icons/fa";
+import { BiCodeBlock } from "react-icons/bi";
+import "./tiptap.scss";
+
+const MenuBar = ({ editor, setLink }) => {
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className="menuBar">
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={editor.isActive('bold') ? 'is-active' : ''}
+      >
+        <FaBold />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className={editor.isActive('italic') ? 'is-active' : ''}
+      >
+        <FaItalic />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleUnderline().run()}
+        className={editor.isActive("underline") ? "is_active" : ""}
+      >
+        <FaUnderline />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        className={editor.isActive('strike') ? 'is-active' : ''}
+      >
+        <FaStrikethrough />
+      </button>
+      <button onClick={setLink} className={editor.isActive('link') ? 'is-active' : ''}>
+        setLink
+      </button>
+      <button
+        onClick={() => editor.chain().focus().unsetLink().run()}
+        disabled={!editor.isActive('link')}
+      >
+        unsetLink
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        className={editor.isActive('code') ? 'is-active' : ''}
+      >
+        <FaCode />
+      </button>
+      <input
+        type="color"
+        onInput={event => editor.chain().focus().setColor(event.target.value).run()}
+        value={editor.getAttributes('textStyle').color}
+      />
+      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+        clear marks
+      </button>
+      {/* <button onClick={() => editor.chain().focus().clearNodes().run()}>
+        clear nodes
+      </button> */}
+      {/* <button
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        className={editor.isActive('paragraph') ? 'is-active' : ''}
+      >
+        paragraph
+      </button> */}
+      <button
+        onClick={() => editor.chain().focus().toggleHighlight().run()}
+        className={editor.isActive('highlight') ? 'is-active' : ''}
+      >
+        toggleHighlight
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHighlight({ color: '#ffa8a8' }).run()}
+        className={editor.isActive('highlight', { color: '#ffa8a8' }) ? 'is-active' : ''}
+      >
+        red
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+      >
+        <FaHeading className="heading1" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+      >
+        <FaHeading className="heading2" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+      >
+        <FaHeading className="heading3" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+      >
+        <FaHeading className="heading4" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+      >
+        <FaHeading className="heading5" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+      >
+        <FaHeading className="heading6" />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive('bulletList') ? 'is-active' : ''}
+      >
+        <FaListUl />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive('orderedList') ? 'is-active' : ''}
+      >
+        <FaListOl />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive('codeBlock') ? 'is-active' : ''}
+      >
+        <BiCodeBlock />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive('blockquote') ? 'is-active' : ''}
+      >
+        <FaQuoteLeft />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('left').run()}
+        className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}
+      >
+        left
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('center').run()}
+        className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}
+      >
+        center
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('right').run()}
+        className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}
+      >
+        right
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setTextAlign('justify').run()}
+        className={editor.isActive({ textAlign: 'justify' }) ? 'is-active' : ''}
+      >
+        justify
+      </button>
+      <button onClick={() => editor.chain().focus().unsetTextAlign().run()}>unsetTextAlign</button>
+      {/* <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        horizontal rule
+      </button> */}
+      {/* <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+        hard break
+      </button> */}
+      <button
+        onClick={() => editor.chain().focus().toggleSubscript().run()}
+        className={editor.isActive('subscript') ? 'is-active' : ''}
+      >
+        <FaSubscript />
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleSuperscript().run()}
+        className={editor.isActive('superscript') ? 'is-active' : ''}
+      >
+        <FaSuperscript />
+      </button>
+      <button onClick={() => editor.chain().focus().undo().run()}>
+        <FaUndo />
+      </button>
+      <button onClick={() => editor.chain().focus().redo().run()}>
+        <FaRedo />
+      </button>
+    </div>
+  )
+}
+
+const Tiptap = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit.configure({
+        paragraph: {
+          HTMLAttributes: {
+            class: 'my-custom-paragraph',
+          },
+        }
+      }),
+      Highlight.configure({
+        multicolor: true,
+      }),
+      Placeholder.configure({
+        // Use a placeholder:
+        placeholder: 'Write something …',
+        // Use different placeholders depending on the node type:
+        // placeholder: ({ node }) => {
+        //   if (node.type.name === 'heading') {
+        //     return 'What’s the title?'
+        //   }
+
+        //   return 'Can you add some further context?'
+        // },
+      }),
+      Link.configure({
+        HTMLAttributes: {
+          class: 'my-custom-class',
+        },
+      }),
+      Underline,
+      Superscript,
+      Subscript,
+      TextStyle,
+      Color,
+      TextAlign.configure({
+        alignments: ['left', 'right', 'center', 'justify'],
+        types: ['heading', 'paragraph'],
+        defaultAlignment: 'left',
+      })
+    ],
+    content: '',
+    autofocus: true,
+    editable: true,
+    injectCSS: false,
+    onUpdate: ({editor}) => {
+      const html = editor.getHTML();
+      const json = editor.getJSON();
+      console.log("HTML: ", html)
+      console.log("Json: ", json)
+    }
+  })
+
+  const setLink = useCallback(() => {
+    const previousUrl = editor.getAttributes('link').href
+    const url = window.prompt('URL', previousUrl)
+
+    // cancelled
+    if (url === null) {
+      return
+    }
+
+    // empty
+    if (url === '') {
+      editor.chain().focus().extendMarkRange('link').unsetLink()
+        .run()
+
+      return
+    }
+
+    // update link
+    editor.chain().focus().extendMarkRange('link').setLink({ href: url })
+      .run()
+  }, [editor])
+
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <div className="textEditor">
+      <MenuBar editor={editor} setLink={setLink} />
+      <EditorContent editor={editor} />
+    </div>
+  )
+}
+
+export default Tiptap;
